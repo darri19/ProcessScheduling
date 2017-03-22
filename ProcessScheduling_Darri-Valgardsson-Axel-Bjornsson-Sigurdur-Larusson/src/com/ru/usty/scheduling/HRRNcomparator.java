c50e48adadc5b51a77b398ffcb6afe384a8f4753
Darri Valgarðsson <DarrMac@clients-pool-1.nat.ru.is>
2017-03-22 15:14:51 +0000

    Making axel happy

--- ProcessScheduling_Darri-Valgardsson-Axel-Bjornsson-Sigurdur-Larusson/src/com/ru/usty/scheduling/HRRNcomparator.java
+++ ProcessScheduling_Darri-Valgardsson-Axel-Bjornsson-Sigurdur-Larusson/src/com/ru/usty/scheduling/HRRNcomparator.java
@@ -13,12 +13,10 @@ public class HRRNcomparator implements Comparator<Integer>{
 	public int compare(Integer arg0, Integer arg1) {
 		long length0 = (s.processExecution.getProcessInfo(arg0).totalServiceTime+s.processExecution.getProcessInfo(arg0).elapsedWaitingTime)/s.processExecution.getProcessInfo(arg0).totalServiceTime;
 		long length1 = (s.processExecution.getProcessInfo(arg1).totalServiceTime+s.processExecution.getProcessInfo(arg1).elapsedWaitingTime)/s.processExecution.getProcessInfo(arg1).totalServiceTime;
-		if (length0 < length1)
-        {
+		
+		if (length0 < length1){
             return 1;
-        }
-        if (length1 < length0)
-        {
+        }else if (length1 < length0){
             return -1;
         }
 		return 0;
--- ProcessScheduling_Darri-Valgardsson-Axel-Bjornsson-Sigurdur-Larusson/src/com/ru/usty/scheduling/SPNcomparator.java
+++ ProcessScheduling_Darri-Valgardsson-Axel-Bjornsson-Sigurdur-Larusson/src/com/ru/usty/scheduling/SPNcomparator.java
@@ -13,11 +13,10 @@ public class SPNcomparator implements Comparator<Integer>{
 	public int compare(Integer arg0, Integer arg1) {
 		long length0 = s.processExecution.getProcessInfo(arg0).totalServiceTime;
 		long length1 = s.processExecution.getProcessInfo(arg1).totalServiceTime;
-		if (length0 < length1)
-        {
+		
+		if (length0 < length1){
             return -1;
-        }
-        if (length1 < length0)
+        }else if(length1 < length0)
         {
             return 1;
         }
